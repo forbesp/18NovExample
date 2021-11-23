9db50409b42836fb83e80d6d9660a31c10b8f560
Paula Forbes <paula.forbes@live.com.au>
2021-11-23 14:58:32 +1100

    first commit of test example

---
+++ 18Nov.js
@@ -0,0 +1,6 @@
+const firstName = "Byronius";
+const clanName = "Karbitus";
+const commonName = "Maris";
+let fullName;
+
+fullName = `${firstName} ${clanName} ${commonName}`; //=> "Byronius Karbitus Maris"
